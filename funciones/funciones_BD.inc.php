e22d1fb40848c544d32e03a79edd0a1c562368d6
Mauro Sego <mauro.segovia.7@gmail.com>
2020-06-15 00:47:19 -0300

    Armada la base para la busqueda del pedido, falta agregar datos al select y mostrar el registro en pantalla

--- funciones/funciones_BD.inc.php
+++ funciones/funciones_BD.inc.php
@@ -9,7 +9,7 @@ function listar_producto_poco_stock(){
 	$limiteStock = 10;
 
 	if($linkConexion != false){
-		$SQL = "SELECT idProducto, dscProducto, stock FROM producto where stock <= '$limiteStock'order by stock desc;";
+		$SQL = "SELECT idProducto, dscProducto, stock FROM producto where stock <= '$limiteStock' order by stock desc;";
 
 		$result = mysqli_query($linkConexion, $SQL);
 		$i = 0;
@@ -21,6 +21,29 @@ function listar_producto_poco_stock(){
 		}
 	}
 	return $listado;
+}
+
+function buscarPedido($nroPedido){
+	$arrayPedido = array();
+	$linkConexion = conexionBD();
+
+	if($linkConexion != false){
+		$SQL = "SELECT idPedido, idProducto, cantidad, idEstadoPedido, idPersona, idDomicilio from pedido where idPedido = '$nroPedido'";
+
+		$result = mysqli_query($linkConexion, $SQL);
+		$i = 0; 
+		while ($data = mysqli_fetch_array($result)){
+			$arrayPedido[$i]['IDPEDIDO'] = $data['idPedido'];
+			$arrayPedido[$i]['IDPRODUCTO'] = $data['idProducto'];
+			$arrayPedido[$i]['CANTIDAD'] = $data['cantidad'];
+			$arrayPedido[$i]['IDESTADOPEDIDO'] = $data['idEstadoPedido'];
+			$arrayPedido[$i]['IDPERSONA'] = $data['idPersona'];
+			$arrayPedido[$i]['IDDOMICILIO'] = $data['idDomicilio'];
+			$i++;
+		}
+	}
+	return $arrayPedido;
+	/*nropedido, producto, cantidad, estado, cliente, domicilio*/
 }
 
 ?>
