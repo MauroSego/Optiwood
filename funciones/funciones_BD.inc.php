adab8d052c68a3a79ac2fb608012d26d96e36841
Mauro Sego <mauro.segovia.7@gmail.com>
2020-06-16 01:16:58 -0300

    Funciona la consulta de pedidos, verificar domicilio completo

--- funciones/funciones_BD.inc.php
+++ funciones/funciones_BD.inc.php
@@ -28,17 +28,17 @@ function buscarPedido($nroPedido){
 	$linkConexion = conexionBD();
 
 	if($linkConexion != false){
-		$SQL = "SELECT idPedido, idProducto, cantidad, idEstadoPedido, idPersona, idDomicilio from pedido where idPedido = '$nroPedido'";
-
+		/*$SQL = "SELECT idPedido, idProducto, cantidad, idEstadoPedido, idPersona, idDomicilio from pedido where idPedido = '$nroPedido'";*/
+		$SQL = "SELECT t1.idPedido, t2.dscProducto, t1.cantidad, t3.dscEstado, concat(t4.nombre, ' ', t4.apellido) as cliente, concat(t5.calle, ' ', t5.altura) as domicilio from pedido t1 join producto t2 on t1.idProducto = t2.idProducto join estados_pedido t3 on t1.idEstadoPedido = t3.idEstado join persona t4 on t1.idPersona = t4.idPersona join domicilio t5 on t1.idDomicilio = t5.idDomicilio where idPedido = '$nroPedido'";
 		$result = mysqli_query($linkConexion, $SQL);
 		$i = 0; 
 		while ($data = mysqli_fetch_array($result)){
 			$arrayPedido[$i]['IDPEDIDO'] = $data['idPedido'];
-			$arrayPedido[$i]['IDPRODUCTO'] = $data['idProducto'];
+			$arrayPedido[$i]['IDPRODUCTO'] = $data['dscProducto'];
 			$arrayPedido[$i]['CANTIDAD'] = $data['cantidad'];
-			$arrayPedido[$i]['IDESTADOPEDIDO'] = $data['idEstadoPedido'];
-			$arrayPedido[$i]['IDPERSONA'] = $data['idPersona'];
-			$arrayPedido[$i]['IDDOMICILIO'] = $data['idDomicilio'];
+			$arrayPedido[$i]['IDESTADOPEDIDO'] = $data['dscEstado'];
+			$arrayPedido[$i]['IDPERSONA'] = $data['cliente'];
+			$arrayPedido[$i]['IDDOMICILIO'] = $data['domicilio'];
 			$i++;
 		}
 	}
