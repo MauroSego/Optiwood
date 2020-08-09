e241e180654d1a86ea01e52e6f161115801cf2da
Mauro Sego <mauro.segovia.7@gmail.com>
2020-08-08 23:03:01 -0300

    Se muestra el formulario, faltan funciones

---
+++ venta/ventasDeProducto.php
@@ -0,0 +1,48 @@
+<?php
+	require_once '../funciones/conexion.inc.php';
+	require_once '../funciones/funciones_BD.inc.php';
+?>
+<div class="row">
+  <div class="col-sm-12">
+      <form id="frmVentaProductos">
+        <!--Cliente-->
+        <label for="idCliente">Cliente</label>
+        <?php 
+        $linkConexion = conexionBD();
+        $consultaCliente = "SELECT id_cliente, nombre, apellido FROM cliente where activo=1 order by apellido asc";
+        $rsCliente=mysqli_query($linkConexion, $consultaCliente);
+        ?> 
+        <select class="form-control" name="idCliente" id="idCliente">
+          <option value="A">Selecciona</option>
+          <option value="0">Sin Cliente</option>
+          <?php 
+          while($data = mysqli_fetch_array($rsCliente)){
+              echo "<option value='".$data['id_cliente']."'>".$data['apellido'] . ' '. $data['nombre']."</option>";
+          }
+          ?>
+        </select>
+        <!--Producto-->
+        <?php
+        $consultaProducto = "SELECT id_producto, dsc_producto, precio, stock FROM producto where activo=1 order by dsc_producto asc";
+        $rsProducto=mysqli_query($linkConexion, $consultaProducto)
+        ?>
+        <label for="idProducto">Producto</label>
+        <select class="form-control" name="idProducto" id="idProducto">
+          <option value="-1">Selecciona</option>
+          <?php 
+          while($data = mysqli_fetch_array($rsProducto)){
+              echo "<option value='".$data['id_producto']."'>".$data['dsc_producto']."</option>";
+          }
+          ?>
+        </select>
+        <label for="precio">Precio</label>
+        <input type="text" readonly="" class="form-control input-sm" id="precio" name="precio">
+        <label for="stock">Stock</label>
+        <input type="text" readonly="" class="form-control input-sm" id="stock" name="stock">
+        <span class="btn btn-primary" name="btnAgregarArticulo" id="btnAgregarArticulo">Agregar Producto</span> 
+      </form>      
+	</div>
+</div>
+<div class="col-sm-4">
+  <div id="tablaVentasTempLoad"></div>
+</div>
